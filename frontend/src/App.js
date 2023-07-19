dbb100921633563aabfa58a4c48d90be0ff982ae
Stefan Radulian <stefan.radulian@gmail.com>
2023-07-19 22:14:18 +0200

    Initial push with frontend and backend. Mostly following instructions from https://medium.com/comsystoreply/fullstack-serverless-with-aws-sam-a64adcace19f

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [greeting, setGreeting] = useState();
+  
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(greeting => setGreeting(greeting.message))
+  }, [setGreeting]);
+  
+  return (
+    <div>Greeting: {greeting}</div>
+  );
+} 
+
+export default App;
