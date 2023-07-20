aa5ddf1f66390a0c0edfdc90ac8a1a23cb99214c
Stefan Radulian <stefan.radulian@gmail.com>
2023-07-20 22:46:30 +0200

    CORS

--- backend/hello-world/app.mjs
+++ backend/hello-world/app.mjs
@@ -19,7 +19,7 @@ const REDIRECT_URI = 'https://dv5l7o77wjd33.cloudfront.net/api/hello'; // Set th
 
 export const lambdaHandler = async (event, context) => {
   // const requestOrigin = event.headers.origin;
-  const requestOrigin = "https://dv5l7o77wjd33.cloudfront.net";
+  const requestOrigin = "https://dv5l7o77wjd33.cloudfront.net"; 
   console.log("executing lambda handler, origin: ", requestOrigin);
   const code = event.queryStringParameters?.code;
   
