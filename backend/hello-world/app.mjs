9a0e6d20596f509d330fa3668ed9983774c9d230
Stefan Radulian <stefan.radulian@gmail.com>
2023-07-20 14:56:43 +0200

    experimenting with LinkedIn API

--- backend/hello-world/app.mjs
+++ backend/hello-world/app.mjs
@@ -11,16 +11,62 @@
  * 
  */
 
+const axios = require('axios');
+const querystring = require('querystring');
+
+const LINKEDIN_CLIENT_ID = process.env.LINKEDIN_CLIENT_ID;
+const LINKEDIN_CLIENT_SECRET = process.env.LINKEDIN_CLIENT_SECRET;
+const REDIRECT_URI = 'https://8ddujis8q0.execute-api.us-east-1.amazonaws.com/Prod/api/hello'; // Set this to your Lambda endpoint or API Gateway URL
+
+
 export const lambdaHandler = async (event, context) => {
+    const authorizationUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&state=some-state&scope=r_liteprofile%20r_emailaddress`;
+    console.log("CLIENT ID", LINKEDIN_CLIENT_ID);
+    
+    return {
+      statusCode: 302,
+      headers: {
+        Location: authorizationUrl,
+        'Cache-Control': 'no-cache' // Prevent caching of the redirect response
+      }
+    };
+  };
+  
+  // Step 2: Handle the LinkedIn callback
+  exports.callback = async (event, context) => {
+    const code = event.queryStringParameters.code;
+    console.log(">> in callback", event);
+
+    // Step 3: Exchange the authorization code for an access token
+    const tokenEndpoint = 'https://www.linkedin.com/oauth/v2/accessToken';
+    const tokenData = querystring.stringify({
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: REDIRECT_URI,
+      client_id: LINKEDIN_CLIENT_ID,
+      client_secret: LINKEDIN_CLIENT_SECRET
+    });
+  
     try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'Hello World! This is a lambda handler - v4',
-            })
+      const response = await axios.post(tokenEndpoint, tokenData, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
-    } catch (err) {
-        console.log(err);
-        return err;
+      });
+  
+      // Use the access token from the response
+      const accessToken = response.data.access_token;
+      // Now, you can make API requests to LinkedIn on behalf of the user using the access token
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ access_token: accessToken })
+      };
+    } catch (error) {
+      console.error('Error exchanging authorization code for access token:', error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Failed to obtain access token' })
+      };
     }
 };
